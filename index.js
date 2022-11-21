8cea8a017528384fc73a4cdc0243a15f55e210e4
ishita0207 <74813161+ishita0207@users.noreply.github.com>
2022-11-21 18:57:59 +0530

    go jenkins go

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello , starting with your first project in node js');
 });
 
+app.get('/about', (req, res) => {
+  res.send('Hello , I am the best coder of the world! K U!');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:5000`);
 });
